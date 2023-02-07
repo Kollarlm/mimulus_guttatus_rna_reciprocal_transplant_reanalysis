4536672ef7e5e20265c64195e42a61b65c1033cf
Leslie Kollar.email <lesliemkollar@gmail.com>
2023-02-07 15:58:54 -0500

    stuff

--- quick_nucleotide_div.R
+++ quick_nucleotide_div.R
@@ -2,11 +2,17 @@ library(ggplot2)
 library(tidyverse)
 library(tidyr)
 
+## These files were generated with bwa -> 
 path <- "/Users/lesliekollar/Desktop/mimulus-gould2018-reanalysis"
+setwd(path)
 
-pi.all <- read.table("./data/S1/S1_aligned_S1_gould_WGS_sort_duplicates_bcf.windowed.pi", header=TRUE)
+pi.S1 <- read.table("./data/S1/S1_aligned_S1_gould_WGS_sort_duplicates_bcf.windowed.pi", header=TRUE)
+pi.L1 <- read.table("./data/L1/L1_aligned_L1_gould_WGS_sort_duplicates_bcf.windowed.pi", header=TRUE)
 
-hist(pi.all$PI,br=20)
+
+hist(pi.S1$PI,br=20)
+hist(pi.L1$PI,br=20)
+
 
 boxplot(pi.all$PI,ylab="diversity")
 
@@ -23,6 +29,19 @@ S1.pi.plot <- pi.all %>%
   geom_vline(xintercept = 5998986, color= "blue")+
   geom_vline(xintercept = 6260288, color= "blue")
 
-tiff("S1.pi.plot.png", width = 30, height = 20, units = "cm",res = 300)
+tiff("./figures/S1.pi.plot.png", width = 30, height = 20, units = "cm",res = 300)
 print(S1.pi.plot)
+dev.off()
+
+L1.pi.plot <- pi.L1 %>% 
+  filter(CHROM == "chr8") %>% 
+  ggplot(aes(x=BIN_END, y= PI)) +
+  geom_point()+
+  geom_vline(xintercept = 850429, color="red") +
+  geom_vline(xintercept = 7604769, color="red") +
+  geom_vline(xintercept = 1032334, color= "blue")+
+  geom_vline(xintercept = 1246126, color= "blue")
+
+tiff("./figures/L1.pi.plot.png", width = 30, height = 20, units = "cm",res = 300)
+print(L1.pi.plot)
 dev.off()
